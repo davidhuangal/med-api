b795101b8f1792d5155dd99a95dc32f336afcef9
David Huangal <dbhuangal1204@gmail.com>
2018-06-25 13:43:17 -0500

    Added Prediction resource

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from resources.Hello import Hello
 app = Flask(__name__)
 api = Api(app)
 
-api.add_resource(Hello, '/hello')
+api.add_resource(Hello, 'api/hello')
 
 if __name__ == '__main__':
     app.run(port=3000)
